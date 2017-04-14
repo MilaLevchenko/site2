5ab93444d401f032d1fff5023f561b318587823f
Mila Levchenko <83darcey@mail.ru>
2017-04-14 22:46:41 +0300

    made work block by thumbnail, css, js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Людмила on 14.04.2017.
+ */
+$( document ).ready(function() {
+  $("[rel='tooltip']").tooltip();
+
+  $('.workhidden').hover(
+      function(){
+        $(this).find('.caption').slideDown(250); //.fadeIn(250)
+      },
+      function(){
+        $(this).find('.caption').slideUp(250); //.fadeOut(205)
+      }
+  );
+});
